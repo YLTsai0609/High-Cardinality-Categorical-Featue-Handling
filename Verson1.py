132b15ed1c178914f49b5607a288d117f9a576b2
YLTsai0609 <yltsai0609@gmail.com>
2019-08-08 23:13:12 +0800

    Embedding 30% TBD train NN

--- Verson1.py
+++ Verson1.py
@@ -3,14 +3,21 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 # from lightgbm import LGBMClassifier
+import random
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
 import os
 from pathlib import Path
 import warnings
+from pandas.core.common import SettingWithCopyWarning
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 
 # set seed and path
@@ -37,16 +44,29 @@ display(train.dtypes,
 
 
 # * Data 簡介
+# 這次的示範資料集是從Kaggle上2013年的[Amazon員工訪問權限預測挑戰賽](https://www.kaggle.com/c/amazon-employee-access-challenge)中取得
+# 這個資料集，該資料集收集了Amazon公司中各個員工針對每個資源(例如網頁的logging)的訪問紀錄，當員工屬於能夠取得訪問權限時，系統卻不給訪問，又要向上申請才能取得權限，一來一往浪費的非常多時間，因此這場比賽希望能夠建構模型，減少員工訪問權限所需的人工流程，我們取出5個特徵如下 :
 #
-# > RESOURCE
 #
-# > MGR_ID
+# * Feature (X)
 #
-# > ROLE_FAMILY_DESC
+# > RESOURCE : 資源ID
 #
-# > ROLE_FAMILY
+# > MGR_ID : 員工主管的ID 
 #
-# > ROLE_CODE
+# > ROLE_FAMILY_DESC : 員工類別擴展描述 (例如 軟體工程的零售經理)
+#
+# > ROLE_FAMILY : 員工類別 (例如 零售經理)
+#
+# > ROLE_CODE : 員工角色編碼 (例如 經理)
+#
+# * Target (Y)
+#
+# > ACTION : 
+#
+#  >> 1 : RESOURCE 訪問權限取得
+#  
+#  >> 0 : RESOURCE 禁止訪問
 
 # helper function
 def get_score(model, X, y, X_val, y_val):
@@ -186,13 +206,19 @@ print('Xgboost Target encoding:', ohe_xgb_score)
 # # Regularized Target encoding
 
 # +
-FOLDS = StratifiedKFold(n_splits=5)
+FOLDS = StratifiedKFold(n_splits=5, random_state=SEED)
+VAL_FOLDS = KFold(n_splits=5, random_state=SEED)
+
 data_te_cv_train = X_train.copy()
 data_te_cv_val = X_val.copy()
-oof_list = []
-for n_fold, (trn_idx, val_idx) in enumerate(FOLDS.split(data_te_cv_train, y_train)):
+oof_trn_list = []
+oof_test_list = []
+
+for (trn_idx, val_idx), (_, test_idx) in zip(FOLDS.split(data_te_cv_train, y_train),
+                                             VAL_FOLDS.split(data_te_cv_val)):
     X_train_te, y_train_te = data_te_cv_train.iloc[trn_idx], y_train.iloc[trn_idx]
     X_val_te, y_val_te = data_te_cv_train.iloc[val_idx], y_train.iloc[val_idx]
+    X_test_te = data_te_cv_val.iloc[test_idx]
     
     for feature in X_train_te.columns:
         X_train_te[f'{feature}_te'], X_val_te[f'{feature}_te'] = TargetEncoder(X_train_te,
@@ -202,33 +228,78 @@ for n_fold, (trn_idx, val_idx) in enumerate(FOLDS.split(data_te_cv_train, y_trai
                                                                                min_samples_leaf=3,
                                                                                smoothing=2,
                                                                                verbose=False)
-    oof_list.append(X_val_te.reset_index(drop=True))
-
+        _, X_test_te[f'{feature}_te'] =TargetEncoder(X_train_te,
+                                                    X_test_te,
+                                                    ft=feature,
+                                                    target=y_train_te,
+                                                    min_samples_leaf=3,
+                                                    smoothing=2,
+                                                    verbose=False)
+    
+    oof_trn_list.append(X_val_te.reset_index(drop=True))
+    oof_test_list.append(X_test_te.reset_index(drop=True))
 
 te_col = [feature for feature in data_te_train.columns
                               if feature.endswith('_te')]
 
-data_te_cv_train = pd.concat(oof_list,ignore_index=True)
-
-# +
-# Dealing with validation set
-
-data_te_cv_train[te_col].head()
+data_te_cv_train = pd.concat(oof_trn_list,ignore_index=True)
+data_te_cv_val = pd.concat(oof_test_list, ignore_index=True)
 # -
 
-
-
-
-
-
-
-
-
-
-
+te_cv_logit_score = get_score(LogReg_model, data_te_cv_train[te_col], y_train, data_te_cv_val[te_col], y_val)
+te_cv_xgb_score = get_score(xgb_model, data_te_cv_train[te_col], y_train, data_te_cv_val[te_col], y_val)
+print('Logistic Regression Regularized Target encoding:', te_cv_logit_score)
+print('Xgboost Regularized Target encoding:', te_cv_xgb_score)
 # # Embedding
 
+data_embedding_train = X_train.copy()
+data_embedding_val = X_val.copy()
 
+
+class EmbeddingMapping():
+    """
+    Helper class for handling categorical variables
+    An instance of this class should be defined for each categorical variable we want to use.
+    """
+    def __init__(self, s : 'pd.Series') -> None:
+        # get a list of unique values
+        values = s.unique().tolist()
+        self.feature_name = s.name
+        # Set a dictionary mapping from values to integer value
+        self.embedding_dict = {value: int_value + 1 for int_value, value in enumerate(values)}
+        # The num_values will be used as the input_dim when defining the embedding layer.
+        # we set unseen values as maximum value + 1 
+        self.num_values = len(values) + 1
+
+    def mapping(self,s : 'pd.Series', verbose = True) -> None:
+        tmp_series = s.map(self.embedding_dict)
+        unseen_ratio = round(tmp_series.isnull().sum() / len(s), 3)
+        if verbose:
+            print(f'Feature "{self.feature_name}"')
+            print(f'    encode {self.num_values -1} values to label, {self.num_values} will be the unseen value ')
+            print(f'    unssen ratio is : ', unseen_ratio)
+        return tmp_series.fillna(self.num_values)
+
+
+for feature in data_embedding_train.columns:
+    Mapper = EmbeddingMapping(data_embedding_train[feature])
+    data_embedding_train[f'{feature}'] = Mapper.mapping(data_embedding_train[f'{feature}'])
+    data_embedding_val[f'{feature}'] = Mapper.mapping(data_embedding_val[f'{feature}'])
+
+# +
+LR = .002 # Learning rate
+EPOCHS = 50 # Default number of training epochs (i.e. cycles through the training data)
+hidden_units = (32,4) # Size of our hidden layers
+
+def auc(y_true, y_pred):
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+    keras.backend.get_session().run(tf.local_variables_initializer())
+    return auc
+
+def build_and_train_model(s, target, mapper_class, embedding_dimension=8,  
+                          verbose=2, epochs=EPOCHS):
+    tf.set_random_seed(1); np.random.seed(1); random.seed(1)
+# -
 
 
 
